fa06f7c749541a68e66700a8175d507ca51b1f1a
Arthur <arthur11kolesov@gmail.com>
2022-06-07 13:49:24 +0300

    init fiix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuillModule } from 'ngx-quill';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
@@ -13,16 +12,11 @@ import {MatButtonModule} from '@angular/material/button';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    QuillModule.forRoot({
-      modules: {
-        syntax: true
-      }
-    }),
-    MatCardModule,
-    MatButtonModule
+   BrowserModule,
+   AppRoutingModule,
+   QuillModule.forRoot(),
+   MatButtonModule,
+   MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
