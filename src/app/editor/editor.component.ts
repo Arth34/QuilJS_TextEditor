6cd16e7bff611f00ba92d98bebffb761505081ae
Arthur <arthur11kolesov@gmail.com>
2022-06-15 19:01:13 +0300

    order fix

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -17,13 +17,12 @@ export class EditorComponent implements OnInit {
       [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
       [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
       [{ 'direction': 'rtl' }],                         // text direction
-  
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+
       [{ 'font': [] }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  
       [{ 'align': [] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
   
       ['clean'],                                         // remove formatting button
   
